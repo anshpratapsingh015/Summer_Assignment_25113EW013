0e4bf4f61303fabe0d76d9709b4e7f720f9c0452
anshpratapsingh015 <anshpratapsingh015@gmail.com>
2026-06-04 17:18:59 +0530

    Implement Fibonacci sequence in C
    Fix formatting issues and correct printf statement.

---
+++ DAY 4/fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int a=0,b=1,n,i,c;
+scanf("%d",&n);
+printf(" %d\n %d\n"but it ,a,b);
+for(i=1;i<=n-2;i++){
+c=a+b;
+a=b;
+b=c;
+printf("%d\n",c);
+}
+return 0;
+}
