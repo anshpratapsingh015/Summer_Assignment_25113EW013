5ac1556c4a0174e226843acf94f22128d49e2c79
anshpratapsingh015 <anshpratapsingh015@gmail.com>
2026-06-12 01:14:55 +0530

    Create charactertriangle.c

---
+++ DAY 8/charactertriangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int i,j,n;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++){
+  int ascii=65; 
+for(j=1;j<=i;j++){
+printf("%c",ascii);
+ascii=ascii+1;
+}
+  printf("\n");
+  }
+return 0;
+}
