bfec7068f3c41fae8d1d7870541022de38d66836
anshpratapsingh015 <anshpratapsingh015@gmail.com>
2026-06-12 17:40:33 +0530

    Add Fibonacci function and main program
    Implement Fibonacci sequence calculation and output.

---
+++ DAY 12/functionfibonacci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+void fibonacci(int n){
+                 int i,a=0,b=1,c;
+    
+      for(i=1;i<=n-2;i++){
+                         c=a+b;
+                         a=b;
+                              b=c;
+                           printf("%d\n",c);
+
+      }
+
+}
+
+int main(){
+ int i,n,a=0,b=1;
+scanf("%d",&n);
+  
+printf("%d\n%d\n",a,b);
+  
+
+  fibonacci(n);
+  
+
+return 0;
+}
+ 
