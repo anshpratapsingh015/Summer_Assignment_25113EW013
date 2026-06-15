f65dbe639964111930795a02656e784c95aa8f14
anshpratapsingh015 <anshpratapsingh015@gmail.com>
2026-06-15 18:17:33 +0530

    Implement left rotation of an array in C

---
+++ DAY 15/rotatearrayleft.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int temp = arr[0];
+    for(int i = 0; i < n - 1; i++){
+        arr[i] = arr[i + 1];
+    }
+    arr[n - 1] = temp;
+     printf("Array after left rotation: ");
+      for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }  
+  return 0;
+}
